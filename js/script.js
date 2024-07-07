b6afb634c3e2eaf1ed24d1cf970489c2e0717590
Mikhail Sidorov <mu_sidorov@mail.ru>
2024-07-07 23:00:16 +0300

    чуть доработал

--- js/script.js
+++ js/script.js
@@ -2,13 +2,26 @@ const Form = document.querySelector ('#form');
 
 const TaskInput = document.querySelector ('#formGroupExampleInput2');
 
+const TaskIextInfo = document.querySelector ('#formGroupExampleInput2-info');
+
 const TaskHtmlList = document.querySelector ('#taskManeger-task-info');
 
 Form.addEventListener('submit', function (event) {
     event.preventDefault();
     const TaskIext = TaskInput.value;
+    const TaskIextInfoHtml = TaskIextInfo.value;
     
-    const TaskHTML = `<p> ${TaskIext}</p>`     ;
+    const TaskHTML = `
+                    
+                        <li class="list-group-item">
+                             <b> ${TaskIext}</b>
+                            <p> ${TaskIextInfoHtml} </p>
+                        </li>
+                    `;
 
-    TaskHtmlList.insertAdjacentHTML ('beforeend', TaskHTML)
+    TaskHtmlList.insertAdjacentHTML ('beforeend', TaskHTML);
+
+    TaskInput.value = "";
+    TaskIextInfoHtml = TaskIextInfo.value = "";
+    TaskInput.focus();
 })
